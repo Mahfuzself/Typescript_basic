cf011eaf2ae8de592c160c370dbe5837914b5fc7
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-07-10 12:34:39 +0600

    some updated

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var username = "Mahfuz Alam";
-console.log(username);
+var user_name = "Mahfuz Alam";
+console.log(user_name);
+var U_age = 27;
+console.log(U_age);
+var Gender;
+Gender = "Male";
+console.log(Gender);
--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
-let username = "Mahfuz Alam";
-console.log(username)
+let user_name = "Mahfuz Alam";
+console.log(user_name)
+let U_age = 27
+console.log(U_age)
+let Gender : string
+Gender = "Male";
+console.log(Gender)
